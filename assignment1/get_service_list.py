177b203b3b2849b648eaddadd7021e049f5d97e1
karthikr87 <karthikeyanr.87@gmail.com>
2020-02-15 22:53:33 +0530

    making changes

--- assignment1/get_service_list.py
+++ assignment1/get_service_list.py
@@ -3,8 +3,11 @@ from list_aws_services import AWS_Services
 # initialize the aws_service function
 aws_service = AWS_Services()
 print(aws_service.region)
-ec2_details = aws_service.get_ec2_details()
-print(ec2_details)
 
-rds_details = aws_service.get_rds_details()
-print(rds_details)
+# Uncomment the next two lines to get running ec2 instances details
+# ec2_details = aws_service.get_ec2_details()
+# print(ec2_details)
+
+# Uncomment the next two lines to get running rds instances details
+# rds_details = aws_service.get_rds_details()
+# print(rds_details)
